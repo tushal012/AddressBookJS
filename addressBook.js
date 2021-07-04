16ee8756c67feb84aa58deba0790b91f880b9287
tushal kumar <tushalkashyap11@gmail.com>
2021-07-03 23:26:17 -0700

    Added : to ensure valid contacts are added

--- addressBook.js
+++ addressBook.js
@@ -18,57 +18,109 @@ class Contact {
         this.phoneNumber = params[6];
         this.email = params[7];
     }
-    get firstName() {
-        return this._firstName;
-    }
+
+    // to validate first name should start with captital character
+
+    get firstName() { return this.firstName; }
     set firstName(firstName) {
-        this._firstName = firstName;
+        let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+        if (nameRegex.test(firstName))
+            this._firstName = firstName;
+        else
+            throw "Invalid first Name";
     }
-    get lastName() {
-        return this._lastName;
-    }
+
+     //to validate last name should start with captital character
+     
+
+    get lastName() { return this._lastName; }
     set lastName(lastName) {
-        this._lastName = lastName;
+        let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+        if (nameRegex.test(lastName))
+            this._lastName = lastName;
+        else
+            throw "Invalid  last Name";
     }
-    get address() {
-        return this._address;
-    }
+
+    // to validate address should start with captital letters or small letters
+    
+
+    get address() { return this._address; }
     set address(address) {
-        this._address = address;
+        let addressRegex = RegExp("^[A-Za-z]{4,}$");
+        if (addressRegex.test(address))
+            this._address = address;
+        else
+            throw "Invalid address ";
     }
-    get city() {
-        return this._city;
-    }
+
+     // to validate city should start with captital letters or small letters
+    
+
+    get city() { return this._city; }
     set city(city) {
-        this._city = city;
+        let cityRegex = RegExp("^[A-Za-z]{4,}$");
+        if (cityRegex.test(city))
+            this._city = city;
+        else
+            throw "Invalid city ";
     }
-    get state() {
-        return this._state;
-    }
+
+    // to validate state should start with captital letters or small letters
+     
+
+    get state() { return this._state; }
     set state(state) {
-        this._state = state;
+        let stateRegex = RegExp("^[A-Za-z0-9]{4,}$");
+        if (stateRegex.test(state))
+            this._state = state;
+        else
+            throw "Invalid state";
     }
-    get zip() {
-        return this._zip;
-    }
+
+    // to validate zip  should start with numbers 
+     
+
+    get zip() { return this._zip; }
     set zip(zip) {
-        this._zip = zip;
+        let zipRegex = RegExp("^[1-9]{3}[ ]?[0-9]{3}$");
+        if (zipRegex.test(zip))
+            this._zip = zip;
+        else
+            throw "Invalid zip ";
     }
-    get phoneNumber() {
-        return this._phoneNumber;
-    }
+
+    //  to validate phone number should start with numbers to allow max 10 digit
+     
+
+    get phoneNumber() { return this._phoneNumber; }
     set phoneNumber(phoneNumber) {
-        this._phoneNumber = phoneNumber;
+        let phoneRegex = RegExp("^[1-9]{2}[ ]{1}[0-9]{10}$");
+        if (phoneRegex.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else
+            throw "Invalid phone number";
     }
-    get email() {
-        return this._email;
-    }
+
+    // to validate email id  should start with small letter to allow one number and one
+  
+
+    get email() { return this._email; }
     set email(email) {
-        this._email = email;
+        let emailRegex = RegExp("^([a-z0-9A-Z])+([.]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-zA-Z]{2,}([.]{1}[a-z]{2,})?$");
+        if (emailRegex.test(email))
+            this._email = email;
+        else
+            throw "Invalid email";
     }
+
     toString() {
-        return '\nName : ' + this.firstName + ' ' + this.lastName + '\nAddress : ' + this.address + '\nCity : ' + this.city +
-            '\nState : ' + this.state + '\nZip : ' + this.zip + '\nphoneNumber : ' + this.phoneNumber + '\nEmail : ' + this.email;
+        return "First Name: " + this.firstName + ", Last Name: " + this.lastName
+            + "\nAddress: " + this.address
+            + ", City: " + this.city + ", State: "
+            + this.state + "\nZip: " + this.zip
+            + ", Phone Number: " + this.phoneNumber + ", Email: " + this.email;
     }
 }
+
 console.log("Welcome to address book ");
